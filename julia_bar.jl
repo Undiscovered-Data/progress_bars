12d3acf7f07f8a74260609fb1a6a3531f8f74b6c
Undiscovered-Data <chaserbrummel@gmail.com>
2024-06-14 00:22:41 -0500

    Add files via upload

---
+++ julia_bar.jl
@@ -0,0 +1,14 @@
+
+println("  ***              Percent Complete                 ***")
+println("  |                                                   |")
+println("  0% #################### 50% #################### 100%")
+println("  |                                                   |")
+
+print("  ")
+for a in 1:50
+	print("@")
+	sleep(0.2)
+end
+println()
+
+
