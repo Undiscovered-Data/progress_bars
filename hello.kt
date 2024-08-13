9253c257c7325c9f0fc5737516b889b9f999e5a0
Undiscovered-Data <chaserbrummel@gmail.com>
2024-08-12 22:53:57 -0500

    Add files via upload

---
+++ hello.kt
@@ -0,0 +1,14 @@
+
+fun main(args : Array<String>) {
+	println("   ***                   Percent Complete                 ***")
+	println("   |                                                         |")
+	println("   0%                        50%                         100%")
+	println("   |                                                        |")
+	print("   ")
+	
+	for (nums in 0..59) {
+		Thread.sleep(300)
+		print("*")
+	}
+	print("\n")
+}
